84819c278d316bfb8c39ea436edc84a8b58acd21
Boris Joffe <boris@joff3.com>
2016-08-31 08:28:15 -0700

    update plopfile and main js template

--- plopfile.js
+++ plopfile.js
@@ -3,25 +3,32 @@
 // Plop: https://github.com/amwmedia/plop
 // inquirer prompts: https://github.com/SBoudrias/Inquirer.js/#objects
 
+const { stripEnd } = require('./util')()
+
 module.exports = function (plop) {
 	plop.setGenerator('js', {
 		prompts: [{
 			type: 'input',
 			name: 'filename',
-			message: 'Enter a relative filename/path (without the js extension)',
+			message: 'Enter a relative filename/path:',
 			filter: function (filename) {
 				console.log('entered:', filename)
 
-				if (/\//.test(filename))
-					return filename
-				else
-					return filename + '/index'
+				// create folder with index.js
+				// e.g. 'server/' -> 'server/index.js'
+				if (filename.endsWith('/'))
+					filename = filename + '/index'
+
+				if (filename.endsWith('.js'))
+					filename = stripEnd(filename, '.js')
+
+				return filename
 			},
 		}],
 		actions: [{
 			type: 'add',
 			path: '{{filename}}.js',
-			templateFile: 'templates/js.templ',
+			templateFile: 'plop/templ.js',
 		}],
 	})
 }
